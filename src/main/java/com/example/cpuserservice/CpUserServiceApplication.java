48a2de6d42e39a1e3478ed3a1ae15fbb4a768862
Hassan Raza <mhraza95@gmail.com>
2024-07-15 17:18:29 +0530

    Security and Audit updates
    Disabled default spring default security and enabled JPA Audit.

--- src/main/java/com/example/cpuserservice/CpUserServiceApplication.java
+++ src/main/java/com/example/cpuserservice/CpUserServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.cpuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@EnableJpaAuditing
 public class CpUserServiceApplication {
 
 	public static void main(String[] args) {
