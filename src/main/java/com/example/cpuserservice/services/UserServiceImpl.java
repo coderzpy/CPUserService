4bd8e26e8f6fdfcfac6ee9a9fb9b6763222c3fd1
Hassan Raza <mhraza95@gmail.com>
2024-07-16 17:45:21 +0530

    Logging

--- src/main/java/com/example/cpuserservice/services/UserServiceImpl.java
+++ src/main/java/com/example/cpuserservice/services/UserServiceImpl.java
@@ -4,12 +4,15 @@ import com.example.cpuserservice.dtos.UserRegistrationDto;
 import com.example.cpuserservice.models.User;
 import com.example.cpuserservice.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -17,23 +20,29 @@ public class UserServiceImpl implements UserService {
 
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
     public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    @Override
     public User registerNewUser(UserRegistrationDto userRegistrationDto) {
 
         try {
 
+            log.info("Attempting to register new user with username: {}", userRegistrationDto.getUsername());
             User user = new User();
             user.setUsername(userRegistrationDto.getUsername());
             user.setEmail(userRegistrationDto.getEmail());
 
             user.setPasswordHash(bCryptPasswordEncoder.encode(userRegistrationDto.getPassword()));
+            log.info("Successfully registered new user with username: {}", userRegistrationDto.getUsername());
 
             return userRepository.save(user);
         } catch (DataIntegrityViolationException e) {
+
+            log.error("Error registering new user with username: {}", userRegistrationDto.getUsername(), e);
             throw new DataIntegrityViolationException("Username or email already exists", e);
         }
 
